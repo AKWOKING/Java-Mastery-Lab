cdb6c4538062d51f843ae34129a278454aa409bd
AKWOKING <akwo.king.dev@gmail.com>
2026-04-28 07:57:28 +0100

    Types and variables

---
+++ 01-Syntax-and-Types/GameCharacter.java
@@ -0,0 +1,9 @@
+public class GameCharacter {
+    public static void main(String[] args) {
+        String characterName = "Hero";
+        int healthPoints = 100;
+        double experienceLevel = 1.5;
+        boolean tutorialCompleted = false;
+        System.out.println("Character: " + characterName + "\nHP: " + healthPoints + "\nLevel: " + experienceLevel + "\nTutorial Completed: " + tutorialCompleted + "\n");
+    }
+}
